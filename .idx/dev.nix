5b6704016da1d8386d8e15b693d6ae7597d5e4ae
Fiifi Michaels <118421780+Fiifimichaels@users.noreply.github.com>
2025-08-08 19:15:54 +0000

    Fix Firebase emulator project ID mismatch

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -14,7 +14,7 @@
   # a firebase.json file is written into the user's directory
   services.firebase.emulators = {
     detect = true;
-    projectId = "demo-app";
+    projectId = "preach-it-sms";
     services = ["auth" "firestore"];
   };
   idx = {
